600175b1e2e5132cb5602ed42e7d77825dbac024
aertsst <Stefan.AERTS@ext.ec.europa.eu>
2020-01-15 17:52:45 +0100

    ionic generate page tab1 ionic generate page tab2 ionic generate page tab3

--- ionicTabs/src/app/app-routing.module.ts
+++ ionicTabs/src/app/app-routing.module.ts
@@ -8,6 +8,18 @@ const routes: Routes = [
     path: 'tabs',
     loadChildren: () => import('./tabs/tabs.module').then( m => m.TabsPageModule)
   },
+  {
+    path: 'tab1',
+    loadChildren: () => import('./tab1/tab1.module').then( m => m.Tab1PageModule)
+  },
+  {
+    path: 'tab2',
+    loadChildren: () => import('./tab2/tab2.module').then( m => m.Tab2PageModule)
+  },
+  {
+    path: 'tab3',
+    loadChildren: () => import('./tab3/tab3.module').then( m => m.Tab3PageModule)
+  },
 ];
 
 @NgModule({
